70432504088d8a433c622182e567d07c94d53293
Kareem Ebrahem <safen894@gmail.com>
2023-10-29 01:28:47 +0200

    RE54D1: Disable PRODUCT_STATIC_BOOT_CONTROL_HAL

--- device.mk
+++ device.mk
@@ -21,11 +21,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.ums512
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.ums512 \
-    libgptutils \
-    libz \
-    libcutils
+#PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    #bootctrl.ums512 \
+    #libgptutils \
+    #libz \
+    #libcutils
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
