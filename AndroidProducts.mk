ef2375bc477def0ead6ebf9895aba920fc4e0fbf
Kareem Ebrahem <safen894@gmail.com>
2023-10-29 00:30:27 +0200

    RE54D1: Change omni to twrp in product makefile and lunchers.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_RE54D1.mk
+    $(LOCAL_DIR)/twrp_RE54D1.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_RE54D1-user \
-    omni_RE54D1-userdebug \
-    omni_RE54D1-eng
+    twrp_RE54D1-user \
+    twrp_RE54D1-userdebug \
+    twrp_RE54D1-eng
