a8b2ac4569b9fab33e8bd32c3e9344708b581179
Kareem Ebrahem <safen894@gmail.com>
2023-10-29 00:57:17 +0200

    RE54D1: Update and rename omni_RE54D1.mk to twrp_RE54D1.mk

--- twrp_RE54D1.mk
+++ twrp_RE54D1.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from RE54D1 device
 $(call inherit-product, device/realme/RE54D1/device.mk)
 
 PRODUCT_DEVICE := RE54D1
-PRODUCT_NAME := omni_RE54D1
+PRODUCT_NAME := twrp_RE54D1
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3265
 PRODUCT_MANUFACTURER := realme
